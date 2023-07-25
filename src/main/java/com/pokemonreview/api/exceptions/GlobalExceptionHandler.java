7c3eaf511cd62fa48ecdad777ca0d8848ef4c607
mulzomdao <mulzomdao@gmail.com>
2023-07-25 09:33:41 +0900

    PokemonRepoTest getall method add

--- src/main/java/com/pokemonreview/api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/pokemonreview/api/exceptions/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.pokemonreview.api.exceptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,7 +19,6 @@ public class GlobalExceptionHandler {
         ErrorObject errorObject = new ErrorObject();
         errorObject.setStatusCode(ex.getStatusCode());
         errorObject.setMessage(ex.getMessage());
-        errorObject.setTimestamp(new Date());
 
         return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.resolve(ex.getStatusCode()));
     }
@@ -27,15 +27,22 @@ public class GlobalExceptionHandler {
     protected ResponseEntity<Object> handleException(Exception e) {
         Map<String, Object> result = new HashMap<String, Object>();
         ResponseEntity<Object> ret = null;
-        
-        String msg = "예상치 못한 문제가 발생했습니다.\n관리자에게 연락 하시기 바랍니다.";
-        result.put("message", msg);
+
+        result.put("message", e.getMessage());
         result.put("httpStatus", HttpStatus.INTERNAL_SERVER_ERROR.value());
         ret = new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
         e.printStackTrace();
 
         log.error(e.getMessage(), e);
-
         return ret;
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorObject> accessDeniedExceptionHandler(AccessDeniedException ex) {
+        ErrorObject errorObject = new ErrorObject();
+        errorObject.setStatusCode(HttpStatus.FORBIDDEN.value());
+        errorObject.setMessage(ex.getMessage());
+
+        return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.FORBIDDEN);
     }
 }
